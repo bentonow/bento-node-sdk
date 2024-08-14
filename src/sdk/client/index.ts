fe9ac14904776201c918fd9dc7a43b49987b7351
Martin Donadieu <martindonadieu@gmail.com>
2024-08-14 18:16:43 +0100

    fix: add fallback implementation for others env

--- src/sdk/client/index.ts
+++ src/sdk/client/index.ts
@@ -8,7 +8,22 @@ function encodeBase64(str: string): string {
   } else if (typeof Buffer !== 'undefined') {
     return Buffer.from(str).toString('base64');
   } else {
-    throw new Error('Base64 encoding is not supported in this environment');
+    // Fallback implementation
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    let output = '';
+    for (let block = 0, charCode, i = 0, map = chars;
+         str.charAt(i | 0) || (map = '=', i % 1);
+         output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+
+      charCode = str.charCodeAt(i += 3/4);
+
+      if (charCode > 0xFF) {
+        throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      }
+      
+      block = block << 8 | charCode;
+    }
+    return output;
   }
 }
 
